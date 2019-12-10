fbc9af504aa5beadc280416700ba8f3f4cd71725
zhengbigbig <780357902@qq.com>
2019-12-10 11:11:27 +0800

    JDK动态代理

--- src/main/java/com/Main.java
+++ src/main/java/com/Main.java
@@ -1,14 +1,24 @@
 package com;
 
+import java.lang.reflect.Proxy;
+
 public class Main {
-    static DataService service = new CacheDecorator(new LogDecorator(new DataServiceImpl()));
+    static DataService service = new DataServiceImpl();
 
     public static void main(String[] args) {
+        //
+        /*
+          Proxy有一个reflect和net包，此处使用reflect
+          参数
+            ClassLoader loader,
+            Class<?>[] interfaces,  只能是接口，不然会报错
+             InvocationHandler h  方法拦截后由谁处理
+          下面的DataService是被动态代理的生成的，会被LogProxy拦截，已经不是原先的DataService
 
-        System.out.println(service.a(1));
-        System.out.println(service.b(1));
-
-        System.out.println(service.a(1));
-        System.out.println(service.b(1));
+         */
+        DataService dataService = (DataService) Proxy.newProxyInstance(service.getClass().getClassLoader(),
+                new Class[]{DataService.class},
+                new LogProxy(service));
+        System.out.println(dataService.a(1));
     }
 }
